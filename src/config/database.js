315fe967cd0025b67f1558e68a106fd0713d4c26
franPzTo <franperezt1@gmail.com>
2026-02-04 15:18:39 -0300

    mongoDB configuration

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+const mongoose =require('mongoose')
+
+const connectDB= async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Conección exitosa a la Base de Datos!')
+    } catch (error) {
+        console.error('fallo en la coneción con la base de datos', error.message)
+        process.exist(1);
+    }
+}
+
+module.exports={
+    connectDB
+}
